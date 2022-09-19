6dff989c89328815508a05391e2f82e7ae16c7cc
Pavel Karpov <karpovpaveldm@gmail.com>
2022-09-20 00:25:17 +0300

    Add routes to main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AppRoutes} from "./shared/constants/app-routes";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from './shared/constants/app-routes';
 
 const routes: Routes = [
-   {path: '', pathMatch: 'full', redirectTo: '/register'},
-   {path: AppRoutes.REGISTER_ROUTE, loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
+  {
+    path: AppRoutes.REGISTER_ROUTE,
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: AppRoutes.MAIN_ROUTE,
+    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule),
+  },
 ];
 
 @NgModule({
-   imports: [RouterModule.forRoot(routes)],
-   exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {Store} from "@ngrx/store";
-import {getCurrentUserAction} from "./store/auth/actions/current-user.action";
-import {TranslateService} from "@ngx-translate/core";
-import {environment} from "../environments/environment";
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getCurrentUserAction } from './store/auth/actions/current-user.action';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../environments/environment';
+import { Router } from '@angular/router';
+import { isLoggedIn } from './store/auth/auth.selectors';
+import { AppRoutes } from './shared/constants/app-routes';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +15,19 @@ import {environment} from "../environments/environment";
 export class AppComponent implements OnInit {
 
   constructor(
-      private store: Store,
-      private translateService: TranslateService
+    private store: Store,
+    private translateService: TranslateService,
+    private route: Router,
   ) {
   }
 
   ngOnInit(): void {
     this.store.dispatch(getCurrentUserAction());
+    this.store.select(isLoggedIn).subscribe(
+      (isLogged: boolean | null) => {
+        this.route.navigate([`/${isLogged ? AppRoutes.MAIN_ROUTE : AppRoutes.REGISTER_ROUTE}`]);
+      }
+    );
     this.translateService.use(environment.defaultLocale);
   }
 }
