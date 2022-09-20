60129a9fe74a3f6ee21c0b97888beb30d88c8804
Pavel Karpov <karpovpaveldm@gmail.com>
2022-09-20 14:01:19 +0300

    Create cv-template module

---
+++ src/app/modules/cv-templates/cv-templates.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CvTemplatesRoutingModule } from './cv-templates-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CvTemplatesRoutingModule
+  ]
+})
+export class CvTemplatesModule { }
