e7a5581f3e556a3ef44351ca1b589ef2120b826d
Pavel Karpov <karpovpaveldm@gmail.com>
2022-09-20 15:42:52 +0300

    Add routing to main module

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from '../../shared/constants/app-routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: AppRoutes.EMPLOYEES_ROUTE,
+    loadChildren: () => import('../employees/employees.module').then(m => m.EmployeesModule),
+  },
+  {
+    path: AppRoutes.PROJECTS_ROUTE,
+    loadChildren: () => import('../projects/projects.module').then(m => m.ProjectsModule),
+  },
+  {
+    path: AppRoutes.CV_TEMPLATES_ROUTE,
+    loadChildren: () => import('../cv-templates/cv-templates.module').then(m => m.CvTemplatesModule),
+  },
+  {
+    path: AppRoutes.ENTITIES_ROUTE,
+    loadChildren: () => import('../entities/entities.module').then(m => m.EntitiesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
