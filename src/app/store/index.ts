5d28a0dfddc67b4dddea8f740a28d3f212e86585
Pavel Karpov <karpovpaveldm@gmail.com>
2022-09-20 13:57:47 +0300

    Move Main store to app

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,11 +1,15 @@
 import {AuthStateInterface} from "./auth/models/auth-state.interface";
 import {ActionReducerMap} from "@ngrx/store";
 import {authReducer} from "./auth/auth.reducer";
+import { BreadcrumbsState } from '../shared/models/interfaces/breadcrumbs.state.interface';
+import { breadcrumbsReducer } from './breadcrumbs/breadcrumbs.reducers';
 
 export interface AppState {
   auth: AuthStateInterface,
+  breadcrumbs: BreadcrumbsState,
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
-  auth: authReducer
+  auth: authReducer,
+  breadcrumbs: breadcrumbsReducer,
 }
