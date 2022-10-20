c0363b597a9bb35e9dde8ac458606a0b98573896
Nikolai Nagornykh <mikalai.nahornykh@innowise-group.com>
2022-10-20 09:57:53 +0300

    add style left side navigation

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -6,11 +6,15 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 import { BreadcrumbsModule } from '../../shared/components/breadcrumbs/breadcrumbs.module';
 import { ListboxModule } from 'primeng/listbox';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
