a6ea3bac6d2e6e4e2484029049efff581bd7144e
Nikolai Nagornykh <mikalai.nahornykh@innowise-group.com>
2022-10-20 14:13:12 +0300

    add footer and header to main module

--- src/app/modules/main/pages/header/header.component.ts
+++ src/app/modules/main/pages/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { getCurrentUser } from '@ourStore/auth/auth.selectors';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    public userName: string = '';
 
-  ngOnInit(): void {
-  }
+    private destroy$ = new Subject<void>();
+
+    constructor(private store: Store) {
+    }
+
+    ngOnInit(): void {
+        this.store.pipe(
+            select(getCurrentUser),
+            takeUntil(this.destroy$)
+        ).subscribe((user) => {
+            this.userName = user?.firstName + ' ' + user?.lastName
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
 }
